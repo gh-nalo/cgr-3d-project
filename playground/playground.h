617c0b77e096e9ea3e6795cbd88408b340b4e75a
Andy <andreas.schermann@outlook.de>
2021-01-01 01:37:45 +0100

    Initial commit.

---
+++ playground/playground.h
@@ -0,0 +1,21 @@
+#include <glad/glad.h>
+#include <glfw3.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <playground/stb_image.h>
+#include <playground/shader_s.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void processInput(GLFWwindow* window);
+
+// settings
+const unsigned int SCR_WIDTH = 1920;
+const unsigned int SCR_HEIGHT = 1080;
