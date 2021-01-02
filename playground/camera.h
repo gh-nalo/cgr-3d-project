48ff869c9e0faf5d48e20aee909a75aecdb92e52
Andy-Nalo <Andreas.Schermann@outlook.de>
2021-01-02 17:36:48 +0100

    Refactor:Outsource to cpp files.

--- playground/camera.h
+++ playground/camera.h
@@ -54,46 +54,20 @@ public:
 	/// <param name="yaw">Initial yaw</param>
 	/// <param name="pitch">Initial pitch</param>
 	/// <returns>Obj</returns>
-	Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = YAW, float pitch = PITCH) 
-		: Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
-	{
-		Position = position;
-		WorldUp = up;
-		Yaw = yaw;
-		Pitch = pitch;
-		updateCameraVectors();
-	}
+	Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = YAW, float pitch = PITCH);
 
 	/// <summary>
 	///		Calculate lookAt matrix
 	/// </summary>
 	/// <returns>Updated lookAt matrix</returns>
-	glm::mat4 GetViewMatrix()
-	{
-		return glm::lookAt(Position, Position + Front, Up);
-	}
+	glm::mat4 GetViewMatrix();
 
 	/// <summary>
 	///		Process keyboard input
 	/// </summary>
 	/// <param name="direction">Type of movement</param>
 	/// <param name="deltaTime">Passed time since last frame</param>
-	void ProcessKeyboard(Camera_Movement direction, float deltaTime)
-	{
-		float velocity = MovementSpeed * deltaTime;
-		if (direction == Camera_Movement::FORWARD)
-			Position += glm::vec3(Front.x, 0.0, Front.z) * velocity;
-		if (direction == Camera_Movement::BACKWARD)
-			Position -= glm::vec3(Front.x, 0.0, Front.z) * velocity;
-		if (direction == Camera_Movement::LEFT)
-			Position -= glm::vec3(Right.x, 0.0, Right.z) * velocity;
-		if (direction == Camera_Movement::RIGHT)
-			Position += glm::vec3(Right.x, 0.0, Right.z) * velocity;
-		if (direction == Camera_Movement::UP)
-			Position += glm::vec3(0.0, 1.0, 0.0) * velocity;
-		if (direction == Camera_Movement::DOWN)
-			Position -= glm::vec3(0.0, 1.0, 0.0) * velocity;
-	}
+	void ProcessKeyboard(Camera_Movement direction, float deltaTime);
 
 	/// <summary>
 	///		Process mouse input
@@ -101,57 +75,19 @@ public:
 	/// <param name="xoffset">x offset</param>
 	/// <param name="yoffset">y offset</param>
 	/// <param name="constrainPitch">predefined</param>
-	void ProcessMouseMovement(float xoffset, float yoffset, GLboolean constrainPitch = true)
-	{
-		xoffset *= MouseSensitivity;
-		yoffset *= MouseSensitivity;
-
-		Yaw += xoffset;
-		Pitch += yoffset;
-
-		// Make sure that when pitch is out of bounds, screen doesn't get flipped
-		if (constrainPitch)
-		{
-			if (Pitch > 89.0f)
-				Pitch = 89.0f;
-			if (Pitch < -89.0f)
-				Pitch = -89.0f;
-		}
-
-		// Update Front, Right and Up Vectors using the updated Euler angles
-		updateCameraVectors();
-	}
+	void ProcessMouseMovement(float xoffset, float yoffset, GLboolean constrainPitch = true);
 
 	/// <summary>
 	///		Process scrolling input
 	/// </summary>
 	/// <param name="yoffset">Scrolling offset</param>
-	void ProcessMouseScroll(float yoffset)
-	{
-		Zoom -= (float)yoffset;
-		if (Zoom < 40.0f)
-			Zoom = 40.0f;
-		if (Zoom > 60.0f)
-			Zoom = 60.0f;
-	}
+	void ProcessMouseScroll(float yoffset);
 
 private:
 
 	/// <summary>
 	///		Helper function to update euler angles of the model
 	/// </summary>
-	void updateCameraVectors()
-	{
-		// Calculate the new Front vector
-		glm::vec3 front;
-		front.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
-		front.y = sin(glm::radians(Pitch));
-		front.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
-		Front = glm::normalize(front);
-
-		// Re-calculate the Right and Up vector
-		Right = glm::normalize(glm::cross(Front, WorldUp));  // normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
-		Up = glm::normalize(glm::cross(Right, Front));
-	}
+	void updateCameraVectors();
 };
 #endif
---
+++ playground/shader_m.cpp
@@ -0,0 +1,254 @@
+#include "shader_m.h"
+
+/// <summary>
+///     Constructor
+/// </summary>
+/// <param name="vertexPath">Path to the vertex shader</param>
+/// <param name="fragmentPath">Path to the fragmentshader</param>
+/// <param name="geometryPath">Path to the geometry shader (optional)</param>
+/// <returns>Object</returns>
+Shader::Shader(const char* vertexPath, const char* fragmentPath, const char* geometryPath)
+{
+	// 1. retrieve the vertex/fragment source code from filePath
+	std::string vertexCode;
+	std::string fragmentCode;
+	std::string geometryCode;
+	std::ifstream vShaderFile;
+	std::ifstream fShaderFile;
+	std::ifstream gShaderFile;
+	// ensure ifstream objects can throw exceptions:
+	vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+	fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+	gShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+	try
+	{
+		// open files
+		vShaderFile.open(vertexPath);
+		fShaderFile.open(fragmentPath);
+		std::stringstream vShaderStream, fShaderStream;
+		// read file's buffer contents into streams
+		vShaderStream << vShaderFile.rdbuf();
+		fShaderStream << fShaderFile.rdbuf();
+		// close file handlers
+		vShaderFile.close();
+		fShaderFile.close();
+		// convert stream into string
+		vertexCode = vShaderStream.str();
+		fragmentCode = fShaderStream.str();
+		// if geometry shader path is present, also load a geometry shader
+		if (geometryPath != nullptr)
+		{
+			gShaderFile.open(geometryPath);
+			std::stringstream gShaderStream;
+			gShaderStream << gShaderFile.rdbuf();
+			gShaderFile.close();
+			geometryCode = gShaderStream.str();
+		}
+	}
+	catch (std::ifstream::failure& e)
+	{
+		std::cout << e.what() << std::endl;
+		std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
+	}
+	const char* vShaderCode = vertexCode.c_str();
+	const char* fShaderCode = fragmentCode.c_str();
+	// 2. compile shaders
+	unsigned int vertex, fragment;
+	// vertex shader
+	vertex = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertex, 1, &vShaderCode, NULL);
+	glCompileShader(vertex);
+	checkCompileErrors(vertex, "VERTEX");
+	// fragment Shader
+	fragment = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragment, 1, &fShaderCode, NULL);
+	glCompileShader(fragment);
+	checkCompileErrors(fragment, "FRAGMENT");
+	// if geometry shader is given, compile geometry shader
+	unsigned int geometry;
+	if (geometryPath != nullptr)
+	{
+		const char* gShaderCode = geometryCode.c_str();
+		geometry = glCreateShader(GL_GEOMETRY_SHADER);
+		glShaderSource(geometry, 1, &gShaderCode, NULL);
+		glCompileShader(geometry);
+		checkCompileErrors(geometry, "GEOMETRY");
+	}
+	// shader Program
+	ID = glCreateProgram();
+	glAttachShader(ID, vertex);
+	glAttachShader(ID, fragment);
+	if (geometryPath != nullptr)
+		glAttachShader(ID, geometry);
+	glLinkProgram(ID);
+	checkCompileErrors(ID, "PROGRAM");
+	// delete the shaders as they're linked into our program now and no longer necessery
+	glDeleteShader(vertex);
+	glDeleteShader(fragment);
+	if (geometryPath != nullptr)
+		glDeleteShader(geometry);
+
+}
+
+/// <summary>
+///     Use shader program
+/// </summary>
+void Shader::use()
+{
+	glUseProgram(ID);
+}
+
+/// <summary>
+///     Set uniform in the shader program
+/// </summary>
+/// <param name="name">Name of the uniform</param>
+/// <param name="value">Value of the uniform</param>
+void Shader::setBool(const std::string& name, bool value) const
+{
+	glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
+}
+
+/// <summary>
+///     Set uniform in the shader program
+/// </summary>
+/// <param name="name">Name of the uniform</param>
+/// <param name="value">Value of the uniform</param>
+void Shader::setInt(const std::string& name, int value) const
+{
+	glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+}
+
+/// <summary>
+///     Set uniform in the shader program
+/// </summary>
+/// <param name="name">Name of the uniform</param>
+/// <param name="value">Value of the uniform</param>
+void Shader::setFloat(const std::string& name, float value) const
+{
+	glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+}
+
+/// <summary>
+///     Set uniform in the shader program
+/// </summary>
+/// <param name="name">Name of the uniform</param>
+/// <param name="value">Value of the uniform</param>
+void Shader::setVec2(const std::string& name, const glm::vec2& value) const
+{
+	glUniform2fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+}
+
+/// <summary>
+///     Set uniform in the shader program
+/// </summary>
+/// <param name="name">Name of the uniform</param>
+/// <param name="x">x value of the uniform</param>
+/// <param name="y">y value of the uniform</param>
+void Shader::setVec2(const std::string& name, float x, float y) const
+{
+	glUniform2f(glGetUniformLocation(ID, name.c_str()), x, y);
+}
+
+/// <summary>
+///     Set uniform in the shader program
+/// </summary>
+/// <param name="name">Name of the uniform</param>
+/// <param name="value">Value of the uniform</param>
+void Shader::setVec3(const std::string& name, const glm::vec3& value) const
+{
+	glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+}
+
+/// <summary>
+///     Set uniform in the shader program
+/// </summary>
+/// <param name="name">Name of the uniform</param>
+/// <param name="x">x value of the uniform</param>
+/// <param name="y">y value of the uniform</param>
+/// <param name="z">z value of the uniform</param>
+void Shader::setVec3(const std::string& name, float x, float y, float z) const
+{
+	glUniform3f(glGetUniformLocation(ID, name.c_str()), x, y, z);
+}
+
+/// <summary>
+///     Set uniform in the shader program
+/// </summary>
+/// <param name="name">Name of the uniform</param>
+/// <param name="value">Value of the uniform</param>
+void Shader::setVec4(const std::string& name, const glm::vec4& value) const
+{
+	glUniform4fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+}
+
+/// <summary>
+///     Set uniform in the shader program
+/// </summary>
+/// <param name="name">Name of the uniform</param>
+/// <param name="x">x value of the uniform</param>
+/// <param name="y">y value of the uniform</param>
+/// <param name="z">z value of the uniform</param>
+/// <param name="w">w value of the uniform</param>
+void Shader::setVec4(const std::string& name, float x, float y, float z, float w)
+{
+	glUniform4f(glGetUniformLocation(ID, name.c_str()), x, y, z, w);
+}
+
+/// <summary>
+///     Set uniform in the shader program
+/// </summary>
+/// <param name="name">Name of the uniform</param>
+/// <param name="mat">Value of the uniform</param>
+void Shader::setMat2(const std::string& name, const glm::mat2& mat) const
+{
+	glUniformMatrix2fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+}
+
+/// <summary>
+///     Set uniform in the shader program
+/// </summary>
+/// <param name="name">Name of the uniform</param>
+/// <param name="mat">Value of the uniform</param>
+void Shader::setMat3(const std::string& name, const glm::mat3& mat) const
+{
+	glUniformMatrix3fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+}
+
+/// <summary>
+///     Set uniform in the shader program
+/// </summary>
+/// <param name="name">Name of the uniform</param>
+/// <param name="mat">Value of the uniform</param>
+void Shader::setMat4(const std::string& name, const glm::mat4& mat) const
+{
+	glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+}
+
+/// <summary>
+///     Check for errors with the shader
+/// </summary>
+/// <param name="shader">Shader</param>
+/// <param name="type">Type</param>
+void Shader::checkCompileErrors(GLuint shader, std::string type)
+{
+	GLint success;
+	GLchar infoLog[1024];
+	if (type != "PROGRAM")
+	{
+		glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+		if (!success)
+		{
+			glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+			std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+		}
+	}
+	else
+	{
+		glGetProgramiv(shader, GL_LINK_STATUS, &success);
+		if (!success)
+		{
+			glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+			std::cout << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+		}
+	}
+}
